5443ba9793b23aa63034f3dba0cefb6c52a54d62
Istvan Fodor <fodori@gmail.com>
2022-02-21 22:10:19 +0000

    fix subap redux issue

--- packages/reactNative/src/ReduxProviders.tsx
+++ packages/reactNative/src/ReduxProviders.tsx
@@ -1,16 +1,35 @@
 import React, { ReactNode } from 'react'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-import storeConfigure from './store/store'
+import { createStore, Store, AnyAction } from 'redux'
+import { persistConfig, storeReducers } from '@jsonui/core'
+import storage from '@react-native-async-storage/async-storage'
+import { Persistor } from 'redux-persist/es/types'
+import { persistStore, persistReducer } from 'redux-persist'
 
-function Providers({ children }: { children: ReactNode }) {
-  const { store, persistor } = storeConfigure
-  return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        {children}
-      </PersistGate>
-    </Provider>
-  )
+class Providers extends React.Component {
+  store: Store<any, AnyAction>
+
+  persistor: Persistor
+
+  constructor(props: { children: ReactNode }) {
+    super(props)
+    const reducerConfig = { ...persistConfig, storage } // ersistConfig<CombinedState< root: any;}>, any, any, any>
+    const persistedReducer = persistReducer(reducerConfig, storeReducers)
+    // eslint-disable-next-line no-underscore-dangle
+    this.store = createStore(persistedReducer)
+    this.persistor = persistStore(this.store)
+  }
+
+  render() {
+    const { children } = this.props
+    return (
+      <Provider store={this.store}>
+        <PersistGate loading={null} persistor={this.persistor}>
+          {children}
+        </PersistGate>
+      </Provider>
+    )
+  }
 }
 export default Providers
