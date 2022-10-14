d3cb052805cb70a2b9bed348d03b488bb6763e0e
Istvan Fodor <fodori@gmail.com>
2022-10-14 21:29:02 +0100

    add icon component

--- packages/comp-mui/src/index.tsx
+++ packages/comp-mui/src/index.tsx
@@ -2,9 +2,10 @@ import Button, { ButtonProps } from './components/Button'
 import TextField, { TextFieldProps } from './components/TextField'
 import Radio, { RadioProps } from './components/Radio'
 import Select, { SelectProps } from './components/Select'
+import Icon, { IconProps } from './components/Icon'
 
 import Checkbox, { CheckboxProps } from './components/Checkbox'
 
-export { Button, TextField, Checkbox, Radio, Select }
+export { Button, TextField, Checkbox, Radio, Select, Icon }
 
-export type { ButtonProps, TextFieldProps, CheckboxProps, RadioProps, SelectProps }
+export type { ButtonProps, TextFieldProps, CheckboxProps, RadioProps, SelectProps, IconProps }
