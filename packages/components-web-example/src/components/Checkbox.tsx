14c87056178d3343cc5b72ddbb018f9cfeda1889
Istvan Fodor <fodori@gmail.com>
2022-11-14 15:17:20 +0000

    works

--- packages/components-web-example/src/components/Checkbox.tsx
+++ packages/components-web-example/src/components/Checkbox.tsx
@@ -3,6 +3,12 @@ import MUICheckbox, { CheckboxProps as MUICheckboxProps } from '@mui/material/Ch
 
 export type CheckboxProps = MUICheckboxProps
 
-const Checkbox = (props: CheckboxProps) => <MUICheckbox {...props} />
+const Checkbox = ({ onChange, ...props }: MUICheckboxProps & { onChange: any }) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(event?.target?.checked)
+  }
+
+  return <MUICheckbox {...props} onChange={handleChange} />
+}
 
 export default Checkbox
--- packages/docs-site/src/pages/allInOneExample/AllInOne.tsx
+++ packages/docs-site/src/pages/allInOneExample/AllInOne.tsx
@@ -1,29 +1,14 @@
 import React from 'react'
-import { Grid, Paper, Typography } from '@mui/material'
 
 import { JsonUI } from '@jsonui/react'
 import { axios } from '@jsonui/functions-example'
-import { Button, TextField, Checkbox, Radio, Select, Icon, Slider, Switch, Tooltip } from '@jsonui/components-web-example'
+import { Button, TextField, Radio, Select, Checkbox, Icon, Slider, Switch, Tooltip } from '@jsonui/components-web-example'
 import model from './model.json'
 import '@jsonui/components-web-example/dist/style.css'
 
 function AllInOne() {
   return (
-    <>
-      <Typography variant="h6">Components and functions integrations example</Typography>
-      <Grid container spacing={2} direction="column" justifyContent="center" alignItems="stretch">
-        <Grid item xs={12}>
-          <Paper elevation={3} sx={{ p: 1 }}>
-            <JsonUI
-              model={model}
-              disabledPersist
-              functions={{ axios }}
-              components={{ Button, TextField, Checkbox, Radio, Select, Icon, Slider, Switch, Tooltip }}
-            />
-          </Paper>
-        </Grid>
-      </Grid>
-    </>
+    <JsonUI model={model} disabledPersist functions={{ axios }} components={{ Button, TextField, Checkbox, Radio, Select, Icon, Slider, Switch, Tooltip }} />
   )
 }
 
