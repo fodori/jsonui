3ca462ac39880e520e28ab2828578cc74043e198
Istvan Fodor <fodori@gmail.com>
2023-11-26 19:31:12 +0000

    added multichild stories

---
+++ packages/react/src/stories/MultiChild.stories.tsx
@@ -0,0 +1,73 @@
+import React, { useContext } from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { StockContext, wrapperUtil, constants as c } from '@jsonui/core'
+
+import { JsonUI } from '../index'
+
+const JsonUIStory = {
+  title: 'Multi Child',
+  component: JsonUI,
+} as ComponentMeta<typeof JsonUI>
+
+const Template: ComponentStory<typeof JsonUI> = (args) => <JsonUI {...args} />
+
+function EditMultiChild(props: any) {
+  const stock = useContext(StockContext)
+  const handleChange = (event: any) => {
+    props?.onChange(event.target.value)
+  }
+  const { fieldErrors, validation, value, $childLabel, $childHelperText, onChange, ...ownProps } = props
+  let error = !!fieldErrors
+  let helperText = wrapperUtil.generateNewChildren($childHelperText, stock)
+  if (error && fieldErrors) {
+    helperText = fieldErrors && Array.isArray(fieldErrors) ? fieldErrors.join(', ') : fieldErrors
+  }
+  if (validation && value && validation.jsonataDef) {
+    let isValid = true
+    try {
+      // eslint-disable-next-line global-require, @typescript-eslint/no-var-requires
+      const jsonata = require('jsonata')
+      const expression = jsonata(validation.jsonataDef)
+      isValid = expression.evaluate(value)
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error('jsonata error', validation.jsonataDef, err)
+      isValid = false
+    }
+    if (!isValid) {
+      error = true
+      helperText = validation.fieldErrors && Array.isArray(validation.fieldErrors) ? validation.fieldErrors.join(', ') : validation.fieldErrors
+    }
+  }
+  return (
+    <>
+      <div style={{ fontSize: 20, color: error ? 'red' : undefined }}>{wrapperUtil.generateNewChildren($childLabel, stock)}</div>
+      <input {...ownProps} value={value || ''} onChange={handleChange} />
+      <div style={{ fontSize: 10, color: error ? 'red' : undefined }}>{helperText}</div>
+    </>
+  )
+}
+
+export const MultiChild = Template.bind({})
+MultiChild.args = {
+  model: {
+    $comp: 'EditMultiChild',
+    value: { $modifier: 'get', store: 'data', path: 'firstname' },
+    $childLabel: { $comp: 'Text', $children: 'This is a label', style: { textAlign: 'left', fontSize: 20, margin: 5, color: 'red' } },
+    $childHelperText: { $comp: 'Text', $children: 'JsonUI test page v0.1', style: { textAlign: 'left', fontSize: 10, margin: 5 } },
+    onChange: { $action: 'set', store: 'data', path: 'firstname' },
+  },
+  disabledPersist: true,
+  components: { EditMultiChild },
+}
+
+MultiChild.argTypes = {
+  model: {
+    control: {
+      type: 'object',
+    },
+  },
+  id: { control: { type: 'text' } },
+}
+
+export default JsonUIStory
