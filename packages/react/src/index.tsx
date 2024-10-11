f28334aea8c02e807a67b366d09e9076ae9774a1
Istvan Fodor <fodori@gmail.com>
2024-10-11 19:30:48 +0100

    type fix, need to check

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import { ChangeDefaultValueFuncProp, JSONValue, ViewerProps } from 'types'
+import { JsonUIComponentsType, JsonUIFunctions, JsonUIFunctionType } from '@jsonui/core'
 import ViewerWeb from './ViewerWeb'
 import { MessageHandlerContext, MessageHandler } from './MessageReceiverContext'
 
-type JsonUIProps = ViewerProps
-
-const JsonUI = (props: JsonUIProps) => <ViewerWeb {...props} />
+const JsonUI = (props: ViewerProps) => <ViewerWeb {...props} />
 
 export { JsonUI, MessageHandlerContext, MessageHandler }
-export type { JsonUIProps, JSONValue, ChangeDefaultValueFuncProp }
+export type { ViewerProps as JsonUIProps, JSONValue, ChangeDefaultValueFuncProp, JsonUIFunctions, JsonUIFunctionType, JsonUIComponentsType }
