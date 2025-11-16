e8820560f99c40d6c802fbd681aa98baf799c005
Istvan Fodor <fodori@gmail.com>
2025-11-16 19:19:17 +0000

    storybook works

--- packages/react/.storybook/main.ts
+++ packages/react/.storybook/main.ts
@@ -1,13 +1,20 @@
 import type { StorybookConfig } from '@storybook/react-vite'
+import { mergeConfig } from 'vite'
 
 const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: {
     name: '@storybook/react-vite',
-    options: {
-      // ...
-    },
+    options: {},
   },
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      resolve: {
+        dedupe: ['react', 'react-dom'],
+      },
+    })
+  },
 }
 
 export default config
