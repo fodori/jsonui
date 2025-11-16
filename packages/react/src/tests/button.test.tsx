9c9ceedd504f44cfe19aae3d5132ce1302b487d1
Istvan Fodor <fodori@gmail.com>
2025-11-16 18:13:35 +0000

    added better unit test to test redux store

--- packages/react/src/tests/button.test.tsx
+++ packages/react/src/tests/button.test.tsx
@@ -31,3 +31,183 @@ test('Text component test', () => {
   // After clicking, the text should appear
   expect(screen.getByText('button test')).toBeInTheDocument()
 })
+
+const JSONStringifyValue = ({ value }: any) => {
+  return <h1>value={JSON.stringify(value)}::END</h1>
+}
+
+test('redux value store test: null', () => {
+  render(
+    <JsonUI
+      components={{ JSONStringifyValue }}
+      model={[
+        { $comp: 'JSONStringifyValue', value: { $modifier: 'get', store: 'data', path: '/age' } },
+        {
+          $comp: 'Button',
+          onClick: { $action: 'set', store: 'data', path: '/age', value: null },
+        },
+      ]}
+    />
+  )
+
+  const button = screen.getByRole('button')
+  expect(button).toBeInTheDocument()
+
+  expect(screen.getByText('value=::END')).toBeInTheDocument()
+  fireEvent.click(button)
+  expect(screen.getByText('value=null::END')).toBeInTheDocument()
+})
+
+test('redux value store test: undefined', () => {
+  render(
+    <JsonUI
+      components={{ JSONStringifyValue }}
+      model={[
+        { $comp: 'JSONStringifyValue', value: { $modifier: 'get', store: 'data', path: '/age' } },
+        {
+          $comp: 'Button',
+          onClick: { $action: 'set', store: 'data', path: '/age', value: undefined },
+        },
+      ]}
+    />
+  )
+
+  const button = screen.getByRole('button')
+  expect(button).toBeInTheDocument()
+
+  expect(screen.getByText('value=::END')).toBeInTheDocument()
+  fireEvent.click(button)
+  expect(screen.getByText('value=::END')).toBeInTheDocument()
+})
+
+test('redux value store test: true', () => {
+  render(
+    <JsonUI
+      components={{ JSONStringifyValue }}
+      model={[
+        { $comp: 'JSONStringifyValue', value: { $modifier: 'get', store: 'data', path: '/age' } },
+        {
+          $comp: 'Button',
+          onClick: { $action: 'set', store: 'data', path: '/age', value: true },
+        },
+      ]}
+    />
+  )
+
+  const button = screen.getByRole('button')
+  expect(button).toBeInTheDocument()
+
+  expect(screen.getByText('value=::END')).toBeInTheDocument()
+  fireEvent.click(button)
+  expect(screen.getByText('value=true::END')).toBeInTheDocument()
+})
+
+test('redux value store test: false', () => {
+  render(
+    <JsonUI
+      components={{ JSONStringifyValue }}
+      model={[
+        { $comp: 'JSONStringifyValue', value: { $modifier: 'get', store: 'data', path: '/age' } },
+        {
+          $comp: 'Button',
+          onClick: { $action: 'set', store: 'data', path: '/age', value: false },
+        },
+      ]}
+    />
+  )
+
+  const button = screen.getByRole('button')
+  expect(button).toBeInTheDocument()
+
+  expect(screen.getByText('value=::END')).toBeInTheDocument()
+  fireEvent.click(button)
+  expect(screen.getByText('value=false::END')).toBeInTheDocument()
+})
+
+test('redux value store test: number', () => {
+  render(
+    <JsonUI
+      components={{ JSONStringifyValue }}
+      model={[
+        { $comp: 'JSONStringifyValue', value: { $modifier: 'get', store: 'data', path: '/age' } },
+        {
+          $comp: 'Button',
+          onClick: { $action: 'set', store: 'data', path: '/age', value: 34.342865 },
+        },
+      ]}
+    />
+  )
+
+  const button = screen.getByRole('button')
+  expect(button).toBeInTheDocument()
+
+  expect(screen.getByText('value=::END')).toBeInTheDocument()
+  fireEvent.click(button)
+  expect(screen.getByText('value=34.342865::END')).toBeInTheDocument()
+})
+
+test('redux value store test: string', () => {
+  render(
+    <JsonUI
+      components={{ JSONStringifyValue }}
+      model={[
+        { $comp: 'JSONStringifyValue', value: { $modifier: 'get', store: 'data', path: '/age' } },
+        {
+          $comp: 'Button',
+          onClick: { $action: 'set', store: 'data', path: '/age', value: '34.342865' },
+        },
+      ]}
+    />
+  )
+
+  const button = screen.getByRole('button')
+  expect(button).toBeInTheDocument()
+
+  expect(screen.getByText('value=::END')).toBeInTheDocument()
+  fireEvent.click(button)
+  expect(screen.getByText('value="34.342865"::END')).toBeInTheDocument()
+})
+
+test('redux value store test: array', () => {
+  render(
+    <JsonUI
+      components={{ JSONStringifyValue }}
+      model={[
+        { $comp: 'JSONStringifyValue', value: { $modifier: 'get', store: 'data', path: '/age' } },
+        {
+          $comp: 'Button',
+          onClick: { $action: 'set', store: 'data', path: '/age', value: [null, true, false, 14, 'asd'] },
+        },
+      ]}
+    />
+  )
+
+  const button = screen.getByRole('button')
+  expect(button).toBeInTheDocument()
+
+  expect(screen.getByText('value=::END')).toBeInTheDocument()
+  fireEvent.click(button)
+  expect(screen.getByText('value=[null,true,false,14,"asd"]::END')).toBeInTheDocument()
+})
+
+test('redux value store test: object', () => {
+  render(
+    <JsonUI
+      components={{ JSONStringifyValue }}
+      model={[
+        { $comp: 'JSONStringifyValue', value: { $modifier: 'get', store: 'data', path: '/age' } },
+        {
+          $comp: 'Button',
+          onClick: { $action: 'set', store: 'data', path: '/age', value: { aa: 'aa', bb: 'cc' } },
+        },
+      ]}
+    />
+  )
+
+  const button = screen.getByRole('button')
+  expect(button).toBeInTheDocument()
+
+  expect(screen.getByText('value=::END')).toBeInTheDocument()
+  fireEvent.click(button)
+  expect(screen.getByText('value={"aa":"aa","bb":"cc"}::END')).toBeInTheDocument()
+})
