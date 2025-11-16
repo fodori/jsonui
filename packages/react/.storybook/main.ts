884925dadb0bf4b14ff8eabd0e6a5c3b7825ae63
Istvan Fodor <fodori@gmail.com>
2025-11-16 19:03:46 +0000

    updated storybook

--- packages/react/.storybook/main.ts
+++ packages/react/.storybook/main.ts
@@ -1,22 +1,13 @@
 import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
-  // ...
-  // framework: '@storybook/react-webpack5', 👈 Remove this
-  framework: '@storybook/react-vite', // 👈 Add this
+  framework: {
+    name: '@storybook/react-vite',
+    options: {
+      // ...
+    },
+  },
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 }
 
 export default config
-
-// module.exports = {
-//   "stories": [
-//     "../src/**/*.stories.mdx",
-//     "../src/**/*.stories.@(js|jsx|ts|tsx)"
-//   ],
-//   "addons": [
-//     "@storybook/addon-links",
-//     "@storybook/addon-essentials",
-//     "@storybook/addon-interactions"
-//   ],
-//   "framework": "@storybook/react"
-// }
